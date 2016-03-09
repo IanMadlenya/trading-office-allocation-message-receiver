bc84d37b7b351632963ab491e8af5f64649f1297
Jacek Spólnik <jacek.spolnik@gmail.com>
2016-03-09 19:36:43 +0100

    Register service to Eureka
    - add eureka client dependency
    - add eureka client  configuration

--- src/main/java/com/trading/AllocationMessageReceiverApplication.java
+++ src/main/java/com/trading/AllocationMessageReceiverApplication.java
@@ -14,6 +14,7 @@ import org.springframework.amqp.support.converter.SimpleMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.service.ApiInfo;
@@ -26,6 +27,7 @@ import java.net.URISyntaxException;
 import static springfox.documentation.builders.PathSelectors.regex;
 
 @SpringBootApplication
+@EnableEurekaClient
 @EnableSwagger2
 public class AllocationMessageReceiverApplication {
 
